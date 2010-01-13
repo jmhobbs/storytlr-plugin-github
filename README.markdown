48fb446d5a08a0b10db69a4c204eba622839484c
John Hobbs <john@velvetcache.org>
2010-01-13 16:53:54 -0600

    First version, it works for me.

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# What is this? #
+
+This is a plugin for the Storytlr application.  It allows you to integrate your actions on Github into the Storytlr lifestream.
+
+# Installation #
+
+1) Clone this repo into your storytlr/protected/application/plugins/ directory
+2) Load database.sql into your database
+3) Copy github.png into storytlr/images/
+4) Go on the admin side and add your account
+5) You were done at number 4, you can stop reading now
---
+++ database.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `github_data`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `github_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `content` text,
+  `repository` text,
+  `link` varchar(255) NOT NULL,
+  `published` varchar(45) NOT NULL,
+  PRIMARY KEY  USING BTREE (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`),
+  FULLTEXT KEY `SEARCH` (`title`,`repository`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
